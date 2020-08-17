1cc1af214daa4172617d3ff6f0292edb6d9dc53d
Caio André <caio_andre15@hotmail.com>
2020-08-17 15:46:13 -0300

    Implemented exer003

--- src/course/exercices/files/exer003/Program.java
+++ src/course/exercices/files/exer003/Program.java
@@ -1,4 +1,4 @@
-package course.exercices.files.exer002;
+package course.exercices.files.exer003;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -9,12 +9,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		String path = "c:\\temp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
-		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
+	
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			
 			String line = br.readLine();
 			
@@ -25,19 +21,6 @@ public class Program {
 		}
 		catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 }
