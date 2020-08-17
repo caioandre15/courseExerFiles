e42d7906f30feea7bb0d721d16ac5af087a485d5
Caio André <caio_andre15@hotmail.com>
2020-08-17 15:20:16 -0300

    Implemented exer001

--- src/course/exercices/files/exer001/Program.java
+++ src/course/exercices/files/exer001/Program.java
@@ -1,10 +1,28 @@
 package course.exercices.files.exer001;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
 
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) { // Existe uma nova linha no arquivo ?
+				System.out.println(sc.nextLine());
+			}
+		} 
+		catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		} 
+		finally {
+			if (sc != null) {
+				sc.close();
+			}
+		}
 	}
-
 }
