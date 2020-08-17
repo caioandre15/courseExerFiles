d3c6f688f0e624260866970e0f4ada35ee10fc6c
Caio André <caio_andre15@hotmail.com>
2020-08-17 16:34:16 -0300

    Implemented exer006

---
+++ src/course/exercices/files/exer006/Program.java
@@ -0,0 +1,23 @@
+package course.exercices.files.exer006;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a file path: ");
+		String strPath = sc.nextLine();
+		
+		File path = new File(strPath);
+		
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getParent: " + path.getPath());
+		
+		sc.close();
+	}
+}
